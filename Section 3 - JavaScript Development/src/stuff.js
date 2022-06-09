c504a016d762cf721ff2d0d3292c24edc4ad4e41
Dev Truce [MacOS] <DevTruce@gmail.com>
2022-06-09 15:42:44 -0400

    Completed Lesson 96 [Arrays - Pop - Push - Shift - Unshift]

--- Section 3 - JavaScript Development/src/stuff.js
+++ Section 3 - JavaScript Development/src/stuff.js
@@ -3,5 +3,13 @@ function alertbox() {
   alert("error, please refreash");
 }
 
-// array
+// #### arrays ####
 var fruits = ["Apple", "Bananna", "Orange", "Grape", "Mango"];
+// does not display last item in the array list
+fruits.pop();
+// does not display first item in the array list
+fruits.shift();
+// adds additional element to the end of an arrays item list
+fruits.push("Cherry");
+// adds additional element to the start of an arrays item list
+fruits.unshift("Berry");
